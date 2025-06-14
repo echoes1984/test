f0137aa161fc41d857a4b5b989b56b63621ff209
sleep <remedios03@163.com>
2025-06-14 16:21:40 +0800

    haha

---
+++ build/CMakeFiles/hello.exe.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello.exe.dir/src/hello.cc.o"
+  "CMakeFiles/hello.exe.dir/src/hello.cc.o.d"
+  "hello.exe"
+  "hello.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
